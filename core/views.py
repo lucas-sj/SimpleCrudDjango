e678816438b9d692f07ebcde5a1c650e668574d6
= <=>
2024-04-20 22:41:21 -0300

    csrftoken2

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.middleware.csrf import get_token
 
 def main(request):
     year = 2024
+    csrf_token = get_token(request)
+
     context = {
-        'year': year
+        'year': year,
+        'csrf_token': csrf_token,
     }
+
     return render(request, 'main.html', context)
